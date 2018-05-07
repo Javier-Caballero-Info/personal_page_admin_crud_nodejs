1b3d7ef193720595208700dc18be517011118d2c
Javier Hernán Caballero García <caballerojavier13@gmail.com>
2018-05-07 15:38:52 -0300

    Update index.js

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,11 +1,6 @@
 /*
  * src/schema/index.js
  *
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- *
- * Created by Alex Tan Hong Pin 30/10/2017
- * Copyright (c) 2017 alextanhongpin. All rights reserved.
 **/
 
 import Ajv from 'ajv'
